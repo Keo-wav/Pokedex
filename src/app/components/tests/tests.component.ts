55ee3b7f83c6c6bbd9185f2350e481feb77f4b29
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-07-21 17:00:30 +0200

    added feature to switch on/off the display of BatoSan tests

--- src/app/components/tests/tests.component.ts
+++ src/app/components/tests/tests.component.ts
@@ -1,14 +1,15 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tests',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './tests.component.html',
   styleUrl: './tests.component.css'
 })
 export class TestsComponent {
+  batActivated:boolean = false
   wordsDatabase:string[] = ['thank you', 'arigato', 
                             'damn', 'kuso', 
                             'pervert', 'hentai', 
@@ -36,15 +37,23 @@ export class TestsComponent {
     return tab
   }
 
-  SeparateWords(wordsTab:string[]):string[][] {
-    let tab:string[][] = [[]]
-    for (let i = 0; i < wordsTab.length ; i++) {
-      if (i % 2 === 0) {
-        tab[0].push(wordsTab[i])
-      } else if (i % 2 !== 0) {
-        tab[1].push(wordsTab[i])
-      }
+  // SeparateWords(wordsTab:string[]):string[][] {
+  //   let tab:string[][] = [[]]
+  //   for (let i = 0; i < wordsTab.length ; i++) {
+  //     if (i % 2 === 0) {
+  //       tab[0].push(wordsTab[i])
+  //     } else if (i % 2 !== 0) {
+  //       tab[1].push(wordsTab[i])
+  //     }
+  //   }
+  //   return tab
+  // }
+
+  DisplayBatoSanTests() {
+    if (this.batActivated === false) {
+      this.batActivated = true
+    } else {
+      this.batActivated = false
     }
-    return tab
   }
 }

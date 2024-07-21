9f8f87a93a1971b83f7cf7112377a310b0e3899a
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-07-21 16:28:17 +0200

    more tests for BatoSan, training on ngFor loops

--- src/app/components/tests/tests.component.ts
+++ src/app/components/tests/tests.component.ts
@@ -1,9 +1,10 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tests',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tests.component.html',
   styleUrl: './tests.component.css'
 })
@@ -30,6 +31,18 @@ export class TestsComponent {
     for (let i = 0; i < wordsTab.length ; i++) {
       if (i % 2 !== 0) {
         tab.push(wordsTab[i])
+      }
+    }
+    return tab
+  }
+
+  SeparateWords(wordsTab:string[]):string[][] {
+    let tab:string[][] = [[]]
+    for (let i = 0; i < wordsTab.length ; i++) {
+      if (i % 2 === 0) {
+        tab[0].push(wordsTab[i])
+      } else if (i % 2 !== 0) {
+        tab[1].push(wordsTab[i])
       }
     }
     return tab

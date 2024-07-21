2530fed84061547931124d8ba08b44b81f47886c
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-07-21 15:20:51 +0200

    component generations now appear on index, iteration on generations tab, ngfor test

--- src/app/components/generations/generations.component.ts
+++ src/app/components/generations/generations.component.ts
@@ -1,12 +1,13 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-generations',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './generations.component.html',
   styleUrl: './generations.component.css'
 })
 export class GenerationsComponent {
-
+  generations: string[] = ['Generation I', 'Generation II', 'Generation III', 'Generation IV', 'Generation V', 'Generation VI', 'Generation VI']
 }

45b827ee4c5f50f028e7ec230594fece02449254
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-08-05 12:54:23 +0200

    More styling on the 6 pokemon cards with Bulma css

--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -12,30 +12,34 @@ export class PokemonsComponent {
   pokemons = [
     {
       id:1,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
       name: 'Bulbasaur',
       category: 'Seed',
-      type: ['Plant', 'Poison'],
+      type: ['Grass', 'Poison'],
       height: 70,
       weight: 6.9
     },
     {
       id:2,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
       name: 'Ivysaur',
       category: 'Seed',
-      type: ['Plant', 'Poison'],
+      type: ['Grass', 'Poison'],
       height: 100,
       weight: 13
     },
     {
       id:3,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
       name: 'Venusaur',
       category: 'Seed',
-      type: ['Plant', 'Poison'],
+      type: ['Grass', 'Poison'],
       height: 200,
       weight: 100
     },
     {
       id:4,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
       name: 'Charmander',
       category: 'Lizard',
       type: 'Fire',
@@ -44,6 +48,7 @@ export class PokemonsComponent {
     },
     {
       id:5,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
       name: 'Charmeleon',
       category: 'Flame',
       type: 'Fire',
@@ -52,9 +57,10 @@ export class PokemonsComponent {
     },
     {
       id:6,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
       name: 'Charizard',
       category: 'Flame',
-      type: 'Flight',
+      type: 'Flying',
       height: 170,
       weight: 90.5
     }

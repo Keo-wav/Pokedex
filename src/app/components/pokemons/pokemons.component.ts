30737f40a1aa9460a2b55e8776309c7e2dd31c32
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-08-29 14:32:03 +0200

    added pokemons service so that the pokemons component does not keep the fake database and leaves it to the dedicated service

--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -1,6 +1,7 @@
 import { DecimalPipe, NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokemonComponent } from "../pokemon/pokemon.component";
+import { PokemonsService } from '../../services/pokemons.service';
 
 @Component({
   selector: 'app-pokemons',
@@ -9,63 +10,19 @@ import { PokemonComponent } from "../pokemon/pokemon.component";
   templateUrl: './pokemons.component.html',
   styleUrl: './pokemons.component.css'
 })
-export class PokemonsComponent {
-  pokemons = [
-    {
-      id:1,
-      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-      name: 'Bulbasaur',
-      category: 'Seed',
-      type: ['Grass', 'Poison'],
-      height: 0.70,
-      weight: 6.9
-    },
-    {
-      id:2,
-      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-      name: 'Ivysaur',
-      category: 'Seed',
-      type: ['Grass', 'Poison'],
-      height: 1,
-      weight: 13
-    },
-    {
-      id:3,
-      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      name: 'Venusaur',
-      category: 'Seed',
-      type: ['Grass', 'Poison'],
-      height: 2,
-      weight: 100
-    },
-    {
-      id:4,
-      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-      name: 'Charmander',
-      category: 'Lizard',
-      type: 'Fire',
-      height: 0.60,
-      weight: 8.5
-    },
-    {
-      id:5,
-      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
-      name: 'Charmeleon',
-      category: 'Flame',
-      type: 'Fire',
-      height: 1.1,
-      weight: 19
-    },
-    {
-      id:6,
-      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-      name: 'Charizard',
-      category: 'Flame',
-      type: 'Flying',
-      height: 1.70,
-      weight: 90.5
-    }
-  ]
+export class PokemonsComponent implements OnInit {
 
+  pokemons: any[] = [];
+  pokemonCount: number = 0;
   selectedPokemon: any;
+
+  constructor(private pokemonsService: PokemonsService) {
+
+  }
+
+  ngOnInit() {
+    this.pokemons = this.pokemonsService.GetPokemons();
+    this.pokemonCount = this.pokemonsService.PokemonCount();
+  }
+
 }
---
+++ src/app/services/pokemons.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonsService {
+
+  constructor() { }
+
+  pokemons = [
+    {
+      id:1,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+      name: 'Bulbasaur',
+      category: 'Seed',
+      type: ['Grass', 'Poison'],
+      height: 0.70,
+      weight: 6.9
+    },
+    {
+      id:2,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+      name: 'Ivysaur',
+      category: 'Seed',
+      type: ['Grass', 'Poison'],
+      height: 1,
+      weight: 13
+    },
+    {
+      id:3,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      name: 'Venusaur',
+      category: 'Seed',
+      type: ['Grass', 'Poison'],
+      height: 2,
+      weight: 100
+    },
+    {
+      id:4,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+      name: 'Charmander',
+      category: 'Lizard',
+      type: 'Fire',
+      height: 0.60,
+      weight: 8.5
+    },
+    {
+      id:5,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+      name: 'Charmeleon',
+      category: 'Flame',
+      type: 'Fire',
+      height: 1.1,
+      weight: 19
+    },
+    {
+      id:6,
+      img:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+      name: 'Charizard',
+      category: 'Flame',
+      type: 'Flying',
+      height: 1.70,
+      weight: 90.5
+    }
+  ]
+
+  GetPokemons() {
+    return this.pokemons;
+  }
+
+  PokemonCount() {
+    return this.pokemons.length;
+  }
+}
